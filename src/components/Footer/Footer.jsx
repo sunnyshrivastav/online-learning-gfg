f5f7ec6ab5cf27580aa90766a6a604f4c6ea6741
sunnyshrivastav <hackersunny89@gmail.com>
2024-10-16 20:42:25 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-14 md:gap-4">
           {/* first section */}
           <div className="space-y-4 max-w-[300px]">
-            <h1 className="text-2xl font-bold">The Coding Journey</h1>
+            <h1 className="text-2xl font-bold">online learning platform</h1>
             <p className="text-dark2">
               TCJ is a platform dedicated to empowering aspiring developers.
               From beginner tutorials to advanced programming concepts, we
